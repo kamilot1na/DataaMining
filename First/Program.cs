54a3b0c6585c96ce167ce1b1cecc4b00c13d63c8
Kamil Nazmiev <kamilotina2001@gmail.com>
2020-05-23 12:08:49 +0300

    Initial commit

---
+++ First/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Linq;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataMiningTestt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new VkApi();
+            //autthorization
+            api.Authorize(new ApiAuthParams
+            {
+                ApplicationId = 7479662,
+                Login = "79196861086",
+                Password = "Lublyucs16",
+                Settings = Settings.All,   
+            });
+            Console.WriteLine(1);
+            Console.WriteLine(api.Token);
+
+            var listid = new List<long>();
+            //getting id from your friends
+            var users = api.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            {
+                UserId = 146993560, //your id
+                Fields = ProfileFields.Uid
+            });
+            //adding id's with excepting a locked acc's
+            foreach (var e in users)
+            {
+                if(e.IsClosed == true)   
+                listid.Add(e.Id);
+            }
+            var result = new List<long>();
+            foreach(var e in listid)
+            {
+                var groupes = api.Groups.Get(new GroupsGetParams()
+                {
+                    UserId = e,
+                    Extended = true,
+                    Filter = GroupsFilters.Publics,
+                    Fields = GroupsFields.All
+                }).
+                ToList();
+                var interest = groupes.Where(e => e.Name.Contains("Аниме") || // your themes
+                e.Name.Contains("Dota") ||
+                e.Name.Contains("Програм") ||
+                e.Name.Contains("Кни") ||
+                e.Name.Contains("Сериал"));
+                if (interest.Count() != 0)
+                    result.Add(e);
+            }
+
+            var write = new StreamWriter("C:/DataMining/First/Result.txt");
+
+            foreach (var e in result)
+                write.WriteLine(e);
+
+            write.Close();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Second/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ExcelDataReader;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataminingTestt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = new StreamReader("C:/DataMining/Second/transactions.csv");
+
+            var list = new List<List<string>>();
+            StreamWriter stream = new StreamWriter("C:/DataMining/Second/Result2.csv");
+            string vvv = "";
+            while((vvv = str.ReadLine()) != null)
+            {
+                var count = 0;
+                var temp = new string[2];
+                temp = vvv.Split(';');
+                foreach (var e in list)
+                    if (e[0] == temp[0])
+                    {
+                        e[1] = (int.Parse(e[1])+1).ToString();
+                        count++;
+                    }
+                if (count == 0)
+                    list.Add(new List<string> { temp[0], "0" });
+            }
+            foreach (var e in list.OrderBy(e => e[0]))
+            {
+                if (int.Parse(e[1]) <= 100)
+                    e[1] = (int.Parse(e[1]) * 2).ToString();
+                else
+                    e[1] = (int.Parse(e[1]) - int.Parse(e[1])/3).ToString();
+
+                stream.WriteLine(e[0] + ";" + e[1]);
+
+
+            }
+
+            stream.Close();
+            str.Close();
+            
+        }
+    }
+}
